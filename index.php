48c4a271beba2fc4fd1848b842cb3c1e47b12b14
Gasper Levpuscek <gasp.levp69@gmail.com>
2026-04-28 14:41:57 +0200

    Fix

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+
+$showResults = false;
+
+if (isset($_POST['gambleButton'])) {
+  $_SESSION['p1'] = $_POST['playerName1'];
+  $_SESSION['p2'] = $_POST['playerName2'];
+  $_SESSION['p3'] = $_POST['playerName3'];
+  $n1 = rand(1, 6);
+  $n2 = rand(1, 6);
+  $n3 = rand(1, 6);
+
+  // find highest score
+  $max = max($n1, $n2, $n3);
+
+  // determine winner
+  if ($n1 == $max && $n2 == $max && $n3 == $max) {
+    $winner = "It's a 3-way tie!";
+  } elseif ($n1 == $max && $n2 == $max) {
+    $winner = $_SESSION['p1'] . " and " . $_SESSION['p2'] . " tie!";
+  } elseif ($n1 == $max && $n3 == $max) {
+    $winner = $_SESSION['p1'] . " and " . $_SESSION['p3'] . " tie!";
+  } elseif ($n2 == $max && $n3 == $max) {
+    $winner = $_SESSION['p2'] . " and " . $_SESSION['p3'] . " tie!";
+  } elseif ($n1 == $max) {
+    $winner = "Winner: " . $_SESSION['p1'];
+  } elseif ($n2 == $max) {
+    $winner = "Winner: " . $_SESSION['p2'];
+  } else {
+    $winner = "Winner: " . $_SESSION['p3'];
+  }
+
+  $showResults = true;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -8,33 +45,23 @@
 <body>
   <div class="gameDiv">
     <form method="post">
-      <input type="text" name="playerName1" placeholder="Enter player name 1" required>
-      <input type="text" name="playerName2" placeholder="Enter player name 2" required>
-      <input type="text" name="playerName3" placeholder="Enter player name 3" required>
-      <button type="submit" name="gambleButton">Click me</button>
+      <input type="text" name="playerName1" required>
+      <input type="text" name="playerName2" required>
+      <input type="text" name="playerName3" required>
+
+      <button type="submit" name="gambleButton">Roll</button>
     </form>
   </div>
 
+  <?php if ($showResults): ?>
+    <div class="results">
+      <p><?php echo htmlspecialchars($_SESSION['p1']); ?> rolled: <?php echo $n1; ?></p>
+      <p><?php echo htmlspecialchars($_SESSION['p2']); ?> rolled: <?php echo $n2; ?></p>
+      <p><?php echo htmlspecialchars($_SESSION['p3']); ?> rolled: <?php echo $n3; ?></p>
+
+      <h2><?php echo htmlspecialchars($winner); ?></h2>
+    </div>
+  <?php endif; ?>
 </body>
-
-<?php
-if (isset($_POST['gambleButton'])) {
-
-  $name1 = $_POST['playerName1'];
-  $number1 = rand(1, 6);
-
-  $name2 = $_POST['playerName2'];
-  $number2 = rand(1, 6);
-
-  $name3 = $_POST['playerName3'];
-  $number3 = rand(1, 6);
-
-  echo "<div class='results'>";
-  echo htmlspecialchars($name1) . " scored: " . $number1 . "<br>";
-  echo htmlspecialchars($name2) . " scored: " . $number2 . "<br>";
-  echo htmlspecialchars($name3) . " scored: " . $number3;
-  echo "</div>";
-}
-?>
 
 </html>
